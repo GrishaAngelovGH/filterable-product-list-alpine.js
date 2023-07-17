8e8194366e8eddbd514445e723cf987c63fa7ea2
Grisha Angelov <grisha.angelov89@gmail.com>
2023-07-17 08:50:57 +0300

    Implement Header component

--- src/components/FilterableProductList/FilterableProductList.js
+++ src/components/FilterableProductList/FilterableProductList.js
@@ -1,11 +1,13 @@
 import React from 'jsx-dom'
 
+import Header from 'components/Header'
+
 const FilterableProductList = () => {
 
   return (
     <div class='row g-0 bg-white rounded p-3 shadow'>
-      <div className="col-md-6">
-        <h1>Filterable Product List</h1>
+      <div className='col-md-12'>
+        <Header />
       </div>
     </div>
   )
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'jsx-dom'
+
+const Header = () => (
+  <h3
+    x-data='{title: "Filterable Product List" }'
+    x-text='title'
+    class='bg-primary text-white text-center m-0 p-3 rounded'
+  >
+  </h3>
+)
+
+export default Header
